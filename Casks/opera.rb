6ceb5f47baf6a0787ec8c3cd29d605dd3352e014
Dave Honneffer <pearofducks@gmail.com>
2014-03-05 00:15:35 +0100

    Update Opera to 20

--- Casks/opera.rb
+++ Casks/opera.rb
@@ -1,7 +1,7 @@
 class Opera < Cask
-  url 'http://get.geo.opera.com/pub/opera/desktop/19.0.1326.47/mac/Opera_19.0.1326.47_Setup.dmg'
+  url 'http://get.geo.opera.com/pub/opera/desktop/20.0.1387.64/mac/Opera_20.0.1387.64_Setup.dmg'
   homepage 'http://www.opera.com/'
-  version '19.0.1326.47'
-  sha1 '9732e4c26c62f3ce3f6b0b46a32cb699d45e9230'
+  version '20.0.1387.64'
+  sha1 '828d4eb1a7e8b528b06ca966f0bdbe87e80cb863'
   link 'Opera.app'
 end
